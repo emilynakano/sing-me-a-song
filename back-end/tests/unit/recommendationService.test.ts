49d752b99726e22dcdbf896fd4861905e48030ce
emily <nakanodepaula@gmail.com>
2022-09-24 18:28:15 -0300

    feat: should create a recommendation

--- back-end/tests/unit/recommendationService.test.ts
+++ back-end/tests/unit/recommendationService.test.ts
@@ -10,3 +10,24 @@ beforeEach(() => {
     jest.clearAllMocks();
 });
 
+const recommendation = {
+    name: faker.music.songName(),
+    youtubeLink: `https://www.youtube.com/${faker.random.alphaNumeric(40)}`
+}
+
+describe("recommendation service", () => {
+    it("should create a recommendation", async () => {
+        jest
+            .spyOn(recommendationRepository, "findByName")
+            .mockImplementationOnce((): any => {})
+
+        jest
+            .spyOn(recommendationRepository, "create")
+            .mockImplementationOnce((): any => {})
+
+        await recommendationService.insert(recommendation);
+
+        expect(recommendationRepository.findByName).toBeCalled();
+        expect(recommendationRepository.create).toBeCalled();
+    })
+})
